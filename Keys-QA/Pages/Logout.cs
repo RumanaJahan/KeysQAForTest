32c1d2169ce18e1605dfdaaff5cbaf23c3b669ce
Daniel choi <choiwaim@gmail.com>
2018-10-26 11:07:14 +1100

    Finish logout testing

---
+++ Keys-QA/Pages/Logout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keys_QA.Pages
+{
+    class Logout
+    {
+        // this is from Roopali
+    }
+}
